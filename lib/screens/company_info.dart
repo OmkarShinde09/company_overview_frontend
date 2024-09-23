e6efa69cca783ac636273da0d7f261b1b92dd00b
OmkarWynum <omkar.shinde@pienomial.com>
2024-09-23 16:46:20 +0530

    Loaded the output.json file in the company_info file. Created a model to access the Panel in the company_info file

--- lib/screens/company_info.dart
+++ lib/screens/company_info.dart
@@ -3,9 +3,41 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/widget/first_panel_column.dart';
 import 'package:frontend/widget/second_panedl_dropdown.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
 
-class CompanyInfo extends StatelessWidget {
+class CompanyInfo extends StatefulWidget {
   const CompanyInfo({super.key});
+
+  @override
+  State<CompanyInfo> createState() => _CompanyInfoState();
+}
+
+class _CompanyInfoState extends State<CompanyInfo> {
+  List<Panel> panels = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadPanels();
+  }
+
+  Future<void> loadPanels() async {
+    final String response = await rootBundle.loadString('assets/output.json');
+    final Map<String, dynamic> data = json.decode(response);
+
+    setState(() {
+      // Access the list of panels correctly
+      panels = (data['Article']['Panel'] as List)
+          .map((panel) => Panel.fromJson(panel))
+          .toList();
+
+      print('Panels loaded: ${panels.length}');
+      if (panels.isNotEmpty) {
+        print('First Panel ID: ${panels[0].panelId}');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +73,8 @@ class CompanyInfo extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   //The tiltle of the panel
-                  Text('Acne'),
+                  //Display the First_Text from the first panel's left part
+                  Text(panels[0].left.header),
                   Row(
                     //A row to hold the left and right parts of the panel
                     children: [
@@ -118,13 +151,336 @@ class CompanyInfo extends StatelessWidget {
                     ),
                   ),
 
-                  //This container displays the Sunburst plot
+                  //This container displays the Sunburst plot -> To be added when working with json.
+                ],
+              ),
+            ),
+
+            //Third panel Bull's Eye Plot
+            Container(
+              margin: EdgeInsets.all(28),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //The tiltle of the panel
+                  Text("Bull's Eye Plot"),
+                  Container(
+                    width: 800,
+                    margin: EdgeInsets.only(left: 400),
+                    child: Text(
+                        "The Bullseye chart organizes drugs by their mechanisms of action, highlighting the scientific strategies and innovations behind each therapy. The innermost circle shows all approved drugs, followed by early Phase 1 trials, advanced Phase 2 trials, and nearly market-ready Phase 3 drugs.",
+                        style: TextStyle(fontSize: 16)),
+                  ),
+
+                  //This container displays the DropDowns in the second panel
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SecondPanedlDropdown(
+                          title: 'Select Company',
+                        ),
+                        SecondPanedlDropdown(
+                          title: 'Select Phase',
+                        ),
+                        SecondPanedlDropdown(
+                          title: 'Not yet recruting',
+                        ),
+                        SecondPanedlDropdown(
+                          title: 'Select Drugs',
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  //This container displays the Sunburst plot -> To be added when working with json.
+                ],
+              ),
+            ),
+
+            //Fourth panel which includes the State of Approved Care
+            Container(
+              margin: EdgeInsets.all(28),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //The tiltle of the panel
+                  Text("State of Approved Care"),
+                  Container(
+                    width: 800,
+                    margin: EdgeInsets.only(left: 400),
+                    child: Text(
+                        "The Standard of Care Panel is designed to be your go-to resource for understanding the approved drug regimens across various therapeutic lines, with focus on providing comprehensive and clear information. It aims to support the delivery of effective and personalised patient care.",
+                        style: TextStyle(fontSize: 16)),
+                  ),
+
+                  //This container displays the Sunburst plot -> To be added when working with json.
+                ],
+              ),
+            ),
+
+            //Fifth panel which includes the Landscape of Approved Drugs
+            Container(
+              margin: EdgeInsets.all(28),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //The tiltle of the panel
+                  Text("Landscape of Approved Drugs "),
+                  //This container displays the Sunburst plot -> To be added when working with json.
+                ],
+              ),
+            ),
+
+            //Sixth panel which includes State of Trials of Approved Drugs
+            Container(
+              margin: EdgeInsets.all(28),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //The tiltle of the panel
+                  Text("State of Trials of Approved Drugs"),
+                  //This container displays the Sunburst plot -> To be added when working with json.
+                ],
+              ),
+            ),
+
+            //Seventh panel which includes the State of Trials of Approved Drugs - Graphical Analysis
+            Container(
+              margin: EdgeInsets.all(28),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //The tiltle of the panel
+                  Text(
+                      "State of Trials of Approved Drugs - Graphical Analysis"),
+                  //This container displays the Sunburst plot -> To be added when working with json.
+                ],
+              ),
+            ),
+
+            //Eighth panel which includes the State of Trials of Drugs in Development
+            Container(
+              margin: EdgeInsets.all(28),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //The tiltle of the panel
+                  Text("State of Trials of Drugs in Development"),
+                  //This container displays the Sunburst plot -> To be added when working with json.
+                ],
+              ),
+            ),
+
+            //Ninth panel which includes the State of Trials of Drugs in Development - Graphical Analysis
+            Container(
+              margin: EdgeInsets.all(28),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //The tiltle of the panel
+                  Text("State of Trials of Drugs in Development"),
+                  //This container displays the Sunburst plot -> To be added when working with json.
+                ],
+              ),
+            ),
+
+            //Tenth panel which includes the State of Trials of Drugs in Development - Graphical Analysis Company wise
+            Container(
+              margin: EdgeInsets.all(28),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //The tiltle of the panel
+                  Text(
+                      "State of Trials of Drugs in Development - Graphical Analysis Company wise"),
+                  //This container displays the Sunburst plot -> To be added when working with json.
+                ],
+              ),
+            ),
+
+            //Eleventh panel which includes the Comparison of Trials for Approved Drugs
+            Container(
+              margin: EdgeInsets.all(28),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //The tiltle of the panel
+                  Text("Comparison of Trials for Approved Drugs"),
+                  //This container displays the Sunburst plot -> To be added when working with json.
+                ],
+              ),
+            ),
+
+            //Twelfth panel which includes the Comparison of Trials for Drugs in Development
+            Container(
+              margin: EdgeInsets.all(28),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //The tiltle of the panel
+                  Text("Comparison of Trials for Drugs in Development"),
+                  //This container displays the Sunburst plot -> To be added when working with json.
                 ],
               ),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+// Models
+
+class Panel {
+  final String panelId;
+  final String panelInfo;
+  final Left left;
+  final Right right;
+
+  Panel({
+    required this.panelId,
+    required this.panelInfo,
+    required this.left,
+    required this.right,
+  });
+
+  factory Panel.fromJson(Map<String, dynamic> json) {
+    return Panel(
+      panelId: json['Panel_Id'],
+      panelInfo: json['Panel_Info'] ?? '',
+      left: Left.fromJson(json['Left']),
+      right: Right.fromJson(json['Right']),
+    );
+  }
+}
+
+// Define Left and Right models similarly to how you've done
+
+// Left model
+class Left {
+  final String header;
+  final List<String> firstText;
+  final List<String> secondText;
+  final List<String> thirdText;
+  final List<String> forthText;
+  final List<String>? fifthText;
+  final List<String>? sixthText;
+  final String? linkTitle;
+  final List<Link>? links;
+
+  Left({
+    required this.header,
+    required this.firstText,
+    required this.secondText,
+    required this.thirdText,
+    required this.forthText,
+    this.fifthText,
+    this.sixthText,
+    this.linkTitle,
+    this.links,
+  });
+
+  factory Left.fromJson(Map<String, dynamic> json) {
+    var linkList = json['Links'] as List? ?? [];
+    List<Link> links = linkList.map((i) => Link.fromJson(i)).toList();
+
+    return Left(
+      header: json['Header'],
+      firstText: List<String>.from(json['First_Text']),
+      secondText: List<String>.from(json['Second_Text']),
+      thirdText: List<String>.from(json['Third_Text']),
+      forthText: List<String>.from(json['Forth_Text']),
+      fifthText: json['Fifth_Text'] != null
+          ? List<String>.from(json['Fifth_Text'])
+          : null,
+      sixthText: json['Sixth_Text'] != null
+          ? List<String>.from(json['Sixth_Text'])
+          : null,
+      linkTitle: json['Link_Title'] ?? '',
+      links: links,
+    );
+  }
+}
+
+// Right model
+class Right {
+  final Display display;
+
+  Right({required this.display});
+
+  factory Right.fromJson(Map<String, dynamic> json) {
+    return Right(
+      display: Display.fromJson(json['Display']),
+    );
+  }
+}
+
+// Display model
+class Display {
+  final String displayType;
+  final List<DisplayInfo> displayInfo;
+
+  Display({
+    required this.displayType,
+    required this.displayInfo,
+  });
+
+  factory Display.fromJson(Map<String, dynamic> json) {
+    var displayInfoList = json['Display_Info'] as List;
+    List<DisplayInfo> displayInfos =
+        displayInfoList.map((i) => DisplayInfo.fromJson(i)).toList();
+
+    return Display(
+      displayType: json['Display_Type'],
+      displayInfo: displayInfos,
+    );
+  }
+}
+
+// DisplayInfo model
+class DisplayInfo {
+  final String plot;
+  final int location;
+  final String plotData;
+  final String text;
+  final String title;
+  final String valueType;
+
+  DisplayInfo({
+    required this.plot,
+    required this.location,
+    required this.plotData,
+    required this.text,
+    required this.title,
+    required this.valueType,
+  });
+
+  factory DisplayInfo.fromJson(Map<String, dynamic> json) {
+    return DisplayInfo(
+      plot: json['Plot'],
+      location: json['Location'],
+      plotData: json['Plot_Data'] ?? '',
+      text: json['Text'] ?? '',
+      title: json['Title'] ?? '',
+      valueType: json['Value_Type'] ?? '',
+    );
+  }
+}
+
+// Link model
+class Link {
+  final String link;
+  final String linkText;
+
+  Link({required this.link, required this.linkText});
+
+  factory Link.fromJson(Map<String, dynamic> json) {
+    return Link(
+      link: json['Link'] ?? '',
+      linkText: json['Link_Text'] ?? '',
     );
   }
 }
